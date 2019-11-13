62bfba5f9f240c2c50caf56505a680d4f18326f7
Zack Downs <zjd2035@gmail.com>
2019-11-12 19:12:51 -0700

    Initializing the 3peaks svc layer based off of my graphql-starter

---
+++ test/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/api';
+
+export const users = async () => axios.post(API_URL, {
+  query: `
+  query () {
+    users {
+      email,
+    }
+  }
+  `
+});
